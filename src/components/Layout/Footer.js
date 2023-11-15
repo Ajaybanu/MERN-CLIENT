af2ec92e37ed1ca1640eaba19ec6b4ae6737fba9
Ajaybanu <ajaybanu7@gmail.com>
2023-11-15 21:47:39 +0530

    new one

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <h1 className="text-center">CREATED BY &copy; AJAY BANU</h1>
       <p className="text-center mt-3">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
-        <Link to="/policy">Privacy Policy</Link>
+        <Link to="/policy">Privacy & Policy</Link>
       </p>
     </div>
   );
